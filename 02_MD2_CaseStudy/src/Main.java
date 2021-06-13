95a2d358d803c9f5b7ab16b2af5ce7bb4e041df4
truong20144805 <truongpam95@gmail.com>
2021-06-14 01:59:15 +0700

    90 %

--- 02_MD2_CaseStudy/src/Main.java
+++ 02_MD2_CaseStudy/src/Main.java
@@ -1,48 +1,128 @@
+import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         ManageBook manageBook = new ManageBook();
-       manageBook.addBook();
-            manageBook.display();
+        do {
+            int choice = -1;
+            int opt = -1;
+            displayMainMenu();
+            int choice1 = manageBook.checkInputType(choice);
+            switch (choice1){
+                case 1:
+                    manageBook.addBook();
+                    break;
+                case 2:
+                    manageBook.display();
+                    break;
+                case 3:
+                    List<Book> bookList = manageBook.searchBookById();
+                    if (bookList.size()==0){
+                        System.out.println("No information !");
+                    } else {
+                        Book book = bookList.get(0);
+                        manageBook.displaySearchList();
+                        editMenu();
+                        int opt1 = manageBook.checkInputType(opt);
+                        switch (opt1){
+                            case 1:
+                                manageBook.editName(book);
+                                break;
+                            case 2:
+                                manageBook.editAuthor(book);
+                                break;
+                            case 3:
+                                manageBook.editProducer(book);
+                                break;
+                            case 4:
+                                manageBook.editPage(book);
+                                break;
+                            case 5:
+                                manageBook.editYear(book);
+                                break;
+                            case 6:
+                                manageBook.editPrice(book);
+                                break;
+                            case 7:
+                                manageBook.editInformation(book);
+                                break;
+                            default:
+                                System.out.println("Invalid");
+                        }
+                    }
+                    break;
+                case 4:
+                    manageBook.displaySearchList();
+                    break;
+                case 5:
+                    searchingMenu();
+                    int opt1 = manageBook.checkInputType(opt);
+                    switch (opt1){
+                        case 1:
+                            manageBook.searchBookById();
+                            manageBook.displaySearchList();
+                            break;
+                        case 2:
+                            manageBook.searchBookByName();
+                            manageBook.displaySearchList();
+                            break;
+                        default:
+                            System.out.println("Invalid");
+                    }
+                    break;
+                case 6:
+                    manageBook.sort();
+                    break;
+                case 7:
+                    manageBook.writeToFile();
+                    break;
+                case 8:
+                    try {
+                        manageBook.readFromFile();
+                        manageBook.display();
+                    } catch (IOException e) {
+                        System.out.println("File not found !");;
+                    }
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("wrong input, re input (0 - 6): ");
+            }
+        } while (true);
     }
-    public static void displayMainMenu(){
-                System.out.println("Please enter your choice:\n"+"1.Add new book.\n"+
-                "2.Display booklist.\n"+
-                "3.Find book.\n"+
-                "4.Edit information book.\n"+
-                "5.Delete book.\n"+
-                "6.Sort.\n"+
-                "7.Exit");
-        Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
-        switch (choice){
-            case 1:
 
-                break;
-            case 2:
+    public static void displayMainMenu() {
+        System.out.println("Main menu :\n" + "1.Add new book.\n" +
+                "2.Display book.\n" +
+                "3.Edit book.\n" +
+                "4.Delete book.\n" +
+                "5.Search book.\n" +
+                "6.Sort.\n" +
+                "7.Write to File.\n" +
+                "8.Read to File.\n" +
+                "0.Exit.");
+        System.out.println("Enter your opt: ");
+    }
 
-                break;
-            case 3:
+    public static void searchingMenu() {
+        System.out.println("Searching menu:\n" + "1.Search book by ID.\n" +
+                "2.Search book by name.\n");
+        System.out.println("Enter your opt: ");
+    }
 
-                break;
-            case 4:
-
-                break;
-            case 5:
-
-                break;
-            case 6:
-
-                break;
-            case 7:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Fail ! Please choice again ! Enter to continue...");
-                sc.nextLine();
-                displayMainMenu();
-        }
+    public static void editMenu() {
+        System.out.println("Edit menu :\n" + "1.Edit name.\n" +
+                "2.Edit author.\n" +
+                "3.Edit producer.\n" +
+                "4.Edit page.\n" +
+                "5.Edit year.\n" +
+                "6.Edit price.\n" +
+                "7.Edit all ."
+                );
+        System.out.println("Enter your opt: ");
     }
 }
